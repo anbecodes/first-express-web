90a075d80e364e6c26b109e2bb6667b1e1610947
anbecodes <anbecodes@gmail.com>
2021-05-21 13:16:10 +0200

    change the render

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile)
 
 // middlewares
 app.use(morgan('dev'));
